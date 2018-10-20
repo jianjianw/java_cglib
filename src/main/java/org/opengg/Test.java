1391d021999026a21bc6519f45a3cbf010a9d060
wanglei <13992787306@163.com>
2018-10-20 17:55:20 +0800

    init

---
+++ src/main/java/org/opengg/Test.java
@@ -0,0 +1,19 @@
+package org.opengg;
+
+import net.sf.cglib.beans.BeanGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+
+        List<User> list = new ArrayList<User>();
+
+        BeanGenerator beanGenerator = new BeanGenerator();
+
+    }
+}
